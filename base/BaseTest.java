9c51637beafc45dd45e3818ce8a67fc2d3399687
KANWAL SHEHZADI <137278493+KanwalShehzadi98-ux@users.noreply.github.com>
2025-05-06 19:20:50 +0500

    Create BaseTest.java

---
+++ base/BaseTest.java
@@ -0,0 +1,28 @@
+package base;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class BaseTest {
+	
+	protected static WebDriver driver;
+	
+	@BeforeTest
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("https://www.daraz.pk/");
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		driver.quit();
+	}
+}
+
+
